83816bb289eb18a1914013ca1fcf33054da67c38
unknown <debeaupte@popsicube.fr>
2017-04-19 15:36:19 +0200

    first dealing card logic

---
+++ client/templates/tables/table_page.js
@@ -0,0 +1,19 @@
+Template.tablePage.onCreated(function() {  
+  Session.set('score', 0);
+});
+
+Template.tablePage.helpers(function() {  
+  var score = Session.get('score')[num];
+  return score;
+});
+
+Template.tablePage.events({
+  'click button': function(e) {
+
+  	var tableId = Router.current().params._id;
+
+    Meteor.call('dealCard', tableId, function(error, result) {
+        //console.log(result);
+    });
+  }
+});

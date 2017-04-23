994515d5231249273fe31009d2dc848e580096b5
Vivouskaya <vivou_kurt@hotmail.fr>
2017-04-23 04:33:35 +0200

    design, turns & hands logic try, score render

--- client/templates/tables/table_page.js
+++ client/templates/tables/table_page.js
@@ -1,8 +1,8 @@
 Template.tablePage.onCreated(function() {  
 	var tableId = Router.current().params._id;
 	Meteor.call('addUserOnThisTable', tableId, function(error, result) {
-        return true;
-    });
+      
+  });
 });
 
 Template.tablePage.onDestroyed(function() {  
@@ -12,17 +12,23 @@ Template.tablePage.onDestroyed(function() {
 });
 
 Template.tablePage.helpers({
-	handsPlayed: function() {
+	handsPlayed: function(userId) {
 		var tableId = Router.current().params._id;
-		return Hands.find({tableId: tableId});
+		return Hands.find({tableId: tableId, userId: userId}, {
+        sort: [
+            ["order", "desc"]
+        ]
+    });
 	}
 });
 
+Template.tablePage.rendered = function(){
+  
+};
+
 Template.tablePage.events({
   'click button': function(e) {
-
   	var tableId = Router.current().params._id;
-
     Meteor.call('dealCard', tableId, function(error, result) {
     	//return true;
         //$('.player-card-location').prepend('<div class="card">num:'+result.numero+'<br/>pts: '+result.value+'</div>');
